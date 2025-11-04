b3cf295e5113ccbf241ba9bb0426cc15160ff245
Leonardo Luz Fachel <leonardoluz.f02@gmail.com>
2025-11-03 22:34:21 -0300

    feat: update switch theme script to handle non existing directories

--- scripts/utils/switch-theme.sh
+++ scripts/utils/switch-theme.sh
@@ -23,20 +23,61 @@ ln -nsf "$theme_dir" "$CURRENT_LINK"
 echo "→ Switching to theme: $theme"
 
 # --- Place files in their destinations ---
-ln -sf "$CURRENT_LINK/fzf.zsh" ~/.zshrc.d/styles/fzf.zsh
-ln -sf "$CURRENT_LINK/ghostty.theme" ~/.config/ghostty/themes/ghostty.theme
-ln -sf "$CURRENT_LINK/ghostty_cursor_trail.glsl" ~/.config/ghostty/shaders/ghostty_cursor_trail.glsl
-ln -sf "$CURRENT_LINK/hyprpaper.conf" ~/.config/hypr/hyprpaper.conf
-ln -sf "$CURRENT_LINK/mako" ~/.config/mako/config
-ln -sf "$CURRENT_LINK/tmux_theme.conf" ~/.config/tmux/themes/tmux_theme.conf
-ln -sf "$CURRENT_LINK/waybar.css" ~/.config/waybar/style.css
-ln -sf "$CURRENT_LINK/wofi.css" ~/.config/wofi/style.css
-ln -sf "$CURRENT_LINK/appearance.conf" ~/.config/hypr/hyprland/appearance.conf
-ln -sf "$CURRENT_LINK/ohmyzsh.zsh" ~/.zshrc.d/ohmyzsh/ohmyzsh.zsh
-ln -sf "$CURRENT_LINK/appearance.py" ~/.config/qutebrowser/config/appearance.py
-ln -sf "$CURRENT_LINK/obs-theme.obt" ~/.config/obs-studio/themes/justedit.obt
-ln -sf "$CURRENT_LINK/qt.conf" ~/.config/qt5ct/colors/qt.conf
-ln -sf "$CURRENT_LINK/kvtheme.kvconfig" ~/.local/share/color-schemes/kvtheme/kvtheme.kvconfig
+if [ -d "~/.zshrc.d/styles" ]; then
+  ln -sf "$CURRENT_LINK/fzf.zsh" ~/.zshrc.d/styles/fzf.zsh
+fi
+
+if [ -d "~/.config/ghostty/themes" ]; then
+  ln -sf "$CURRENT_LINK/ghostty.theme" ~/.config/ghostty/themes/ghostty.theme
+fi
+
+if [ -d "~/.config/ghostty/shaders" ]; then
+  ln -sf "$CURRENT_LINK/ghostty_cursor_trail.glsl" ~/.config/ghostty/shaders/ghostty_cursor_trail.glsl
+fi
+
+if [ -d "~/.config/hypr" ]; then
+  ln -sf "$CURRENT_LINK/hyprpaper.conf" ~/.config/hypr/hyprpaper.conf
+fi
+
+if [ -d "~/.config/mako" ]; then
+  ln -sf "$CURRENT_LINK/mako" ~/.config/mako/config
+fi
+
+if [ -d "~/.config/tmux/themes" ]; then
+  ln -sf "$CURRENT_LINK/tmux_theme.conf" ~/.config/tmux/themes/tmux_theme.conf
+fi
+
+if [ -d "~/.config/waybar" ]; then
+  ln -sf "$CURRENT_LINK/waybar.css" ~/.config/waybar/style.css
+fi
+
+if [ -d "~/.config/wofi" ]; then
+  ln -sf "$CURRENT_LINK/wofi.css" ~/.config/wofi/style.css
+fi
+
+if [ -d "~/.config/hypr/hyprland" ]; then
+  ln -sf "$CURRENT_LINK/appearance.conf" ~/.config/hypr/hyprland/appearance.conf
+fi
+
+if [ -d "~/.zshrc.d/ohmyzsh" ]; then
+  ln -sf "$CURRENT_LINK/ohmyzsh.zsh" ~/.zshrc.d/ohmyzsh/ohmyzsh.zsh
+fi
+
+if [ -d "~/.config/qutebrowser/config" ]; then
+  ln -sf "$CURRENT_LINK/appearance.py" ~/.config/qutebrowser/config/appearance.py
+fi
+
+if [ -d "~/.config/obs-studio/themes" ]; then
+  ln -sf "$CURRENT_LINK/obs-theme.obt" ~/.config/obs-studio/themes/justedit.obt
+fi
+
+if [ -d "~/.config/qt5ct/colors" ]; then
+  ln -sf "$CURRENT_LINK/qt.conf" ~/.config/qt5ct/colors/qt.conf
+fi
+
+if [ -d "$HOME/.local/share/color-schemes/kvtheme" ]; then
+  ln -sf "$CURRENT_LINK/kvtheme.kvconfig" ~/.local/share/color-schemes/kvtheme/kvtheme.kvconfig
+fi
 
 # --- Reload / refresh components ---
 
